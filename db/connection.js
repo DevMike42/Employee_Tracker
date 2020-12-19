fdff28c4772577648c90ac25cc8ef0ddec6492af
Mike Rivera <michaelstephenrivera@gmail.com>
2020-12-19 10:15:29 -0600

    Added npm inquirer prompt and added initial prompt functionality to server.js

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: "",
+  database: 'employees'
+})
+
+connection.connect((err) => {
+  if (err) throw err
+  console.log(`Connected as id: ${connection.threadId}`)
+})
+
+module.exports = connection
--- server.js
+++ server.js
@@ -1,8 +1,53 @@
+const logo = require('asciiart-logo')
+const colors = require('colors')
+const { prompt } = require('inquirer')
 
 const init = () => {
-  console.log('Working!')
+  // Renders starting logo
+  const logoText = logo({ name: "Employee Tracker" }).render()
+  console.log(logoText.yellow)
+
+  loadMainPrompts()
 }
 
+// Main Prompts
+const loadMainPrompts = () => {
+  console.log('Welcome to the Employee Tracker!\n'.green.bold)
+
+  prompt([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to do?',
+      choices: [
+        {
+          name: 'View All Employees',
+          value: 'VIEW_ALL_EMPLOYEES'
+        },
+        {
+          name: 'Quit',
+          value: 'QUIT'
+        }
+      ]
+    }
+  ]).then(res => {
+    let userChoice = res.choice
+
+    switch (userChoice) {
+      case 'VIEW_ALL_EMPLOYEES':
+        console.log('You chose View All Employees')
+        break
+
+      default:
+        quit()
+    }
+  })
+}
+
+const quit = () => {
+  console.log('Goodbye!'.green.bold)
+  process.exit()
+}
 
 
 init()
